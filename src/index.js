d9f0445735929a71b8e00fa469812402dd119368
Ruslan0920 <rus_boy2002@ukr.net>
2023-10-12 15:46:33 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ console.log(arrBreedsId);
 fetchBreeds()
   .then(data => {
     data.forEach(element => {
-      console.log(element.id);
+      // console.log(data);
       arrBreedsId.push({ text: element.name, value: element.id });
     });
     new SlimSelect({
@@ -68,3 +68,166 @@ function onFetchError(error) {
     }
   );
 }
+
+
+
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         rating: 8.38,
+//     },
+//     {
+//         title: "На березі спокійних вод",
+//         author: "Роберт Шеклі",
+//         rating: 8.51,
+//     },
+//     {
+//         title: "Сон смішної людини",
+//         author: "Федір Достоєвський",
+//         rating: 7.75,
+//     },
+//     {
+//         title: "Великі сподівання",
+//         author: "Чарльз Діккенс",
+//         rating: 8.75,
+//     },
+// ];
+// console.log(books);
+
+// let massive = [];
+// console.log(massive);
+// console.log(massive[0]);
+// // const markup = massive.map(element => element[0]);
+// // console.log(markup);
+// // cardContainer.innerHTML = markup
+
+
+// // const createObj = books.map(book => `<option value="${book.title}">${book.author}</option>`
+// // ).join(" ");
+// // console.log(createObj);
+// // cardContainer.innerHTML = createObj;
+
+// const addObj = books.forEach(item => {
+//     `<option value="${item.title}">${item.author}</option>`;
+//     console.log(item);
+//     massive.push({ name: item.author, value: item.title });
+//     cardContainer.innerHTML = massive;
+// })
+
+// console.log(addObj);
+
+// const createObj = books.map(book => {`<option value="${book.title}">${book.author}</option>`
+//     // console.log(book);
+//     // console.log(book.author);
+//     // massive.push({ name: book.author, value: book.title });
+//     //     new SlimSelect({
+//     //   select: book.author,
+//     //   data: massive,
+//     // });
+// // return massive
+// });
+// cardContainer.innerHTML = createObj;
+// console.log(createObj);
+
+
+
+// const createObj = books.map(book => {
+// console.log(book);
+// }
+    
+// massive.push({ text: book.name, value: book.id });
+    
+    // new SlimSelect({
+    //   select: cardContainer,
+    //   data: massive,
+    // });
+
+
+    // console.log(book);
+    // massive.push(book.author);
+    // cardContainer.innerHTML = `<option value="">${massive}</option>`;
+    // console.log(cardContainer.textContent);
+    // return massive;
+
+    
+    // return massive;
+    // const allAuthors = book.author;
+    // console.log(allAuthors);
+    // const option = document.createElement('option');
+    // console.log(option);
+    // option.value = book.author;
+    // option.innerHTML = book.title;
+    // console.log(option.innerHTML);
+    // cardContainer.textContent = massive;
+    // console.log(cardContainer.textContent);
+    // cardContainer.innerHTML = `<option value="${idx}">${book.author}</option>`;
+    // new SlimSelect({
+    //   select: cardContainer,
+    //   data: massive,
+    // });
+    // return massive
+    // cardContainer.value = allAuthors;
+    // cardContainer.innerHTML = `<option value="">${massive}</option>`;
+
+
+//     cardContainer.innerHTML = `<option value="">${createObj}</option>`;
+// console.log(createObj);
+
+
+
+// function selectCat(event) {
+//     console.log(event);
+// }
+
+
+
+
+// import axios from "axios";
+// // console.log(axios);
+// axios.defaults.headers.common["x-api-key"] = "live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq";
+
+// import '../src/cat-api.js'
+// import { TheCatAPI } from "@thatapicompany/thecatapi";
+
+
+// const cardContainer = document.querySelector('.breed-select');
+// console.log(cardContainer);
+// const divCatInfo = document.querySelector('.cat-info');
+// console.log(divCatInfo);
+// cardContainer.addEventListener('submit', selectCats)
+
+
+// // const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
+// // console.log(theCatAPI);
+
+
+
+// // fetchBreeds()
+
+
+
+// // function fetchBreeds() {
+// //    return fetch('https://api.thecatapi.com/v1/breeds')
+// //        .then(resp => {
+// //         return resp.json();
+// //     })
+// //     .then(cats => {
+// //         console.log(cats);
+// //         cats.forEach(cat => {
+// //             // console.log(cat.id);
+// //             // console.log(`значення: ${cat.id}, Ім'я: ${cat.name}`);
+// // //   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
+// //             massiveCats.push({ text: cat.name, value: cat.id })
+// //             // cardContainer.value = cat.id;
+// //             // console.log(cardContainer.value);
+            
+// // });
+        
+// //     })
+// //     .catch(error => {
+// //         console.log(error);
+// //     })
+    
+// // }
