99dc795776a44d401ea87d0d3b8d42e12f33a784
Ruslan0920 <rus_boy2002@ukr.net>
2023-10-20 16:08:05 +0300

    111

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ function onSelectBreed(event) {
       loader.classList.replace('loader', 'is-hidden');
       selector.classList.remove('is-hidden');
       const { url, breeds } = data[0];
+      console.log({breeds });
 
       divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
       divCatInfo.classList.remove('is-hidden');
